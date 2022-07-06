12a2bacde34d84b6f05016f1f6c20b2f7a80701f
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-06 20:21:17 +0000

    exporting callback to play.js

---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (key) {
+  process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }  // your code here
+  };
+
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+};
+module.export = { setupInput };

e5f8a27fc52bc478238255088ae003144d1379cd
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-06 20:20:36 +0000

    now pulling from input

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
-const {connect} = require('./client')
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function (key) {
-  process.stdout.write('.');
-    if (key === '\u0003') {
-      process.exit();
-    }  // your code here
-  };
-
-  stdin.on("data", handleUserInput);
-  
-  return stdin;
-};
-setupInput()
+console.log("Connecting ...");
 connect();
+
+setupInput();

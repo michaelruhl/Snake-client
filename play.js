1b46f1a4856cc87948976e0f341afccb17d9bc2b
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-06 20:06:52 +0000

    ctrl c now exits app

--- play.js
+++ play.js
@@ -1,2 +1,20 @@
 const {connect} = require('./client')
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (key) {
+  process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }  // your code here
+  };
+
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+};
+setupInput()
+connect();

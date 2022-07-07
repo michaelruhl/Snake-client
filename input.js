10950bc1efe956d78950fbb4c2b3bcf205c317f6
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-07 00:12:41 +0000

    snek

--- input.js
+++ input.js
@@ -1,17 +1,39 @@
-const setupInput = function () {
+let connection;
+
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  const handleUserInput = function (key) {
-  process.stdout.write('.');
-    if (key === '\u0003') {
-      process.exit();
-    }  // your code here
-  };
 
   stdin.on("data", handleUserInput);
-  
+
   return stdin;
 };
-module.export = { setupInput };
+
+const handleUserInput = function(key) {
+ 
+  if (key === '\u0003' || key === 'x') {
+    process.exit();
+  }  // your code here
+  if (key === 'w') {
+    connection.write("Move: up");
+  };
+  if (key === 'a') {
+    connection.write("Move: left");
+  };
+  if (key === 'd') {
+    connection.write("Move: right");
+  };
+  if (key === 's') {
+    connection.write("Move: down");
+  };
+  if (key === 'y') {
+    connection.write("Say: Gota go sanik fast");
+  }
+
+};
+
+module.exports = { setupInput };
